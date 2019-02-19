df7db15101bc685b66da5d64a8d2c2cebb04012a
Andrii Kost <andriikost@Andriis-MacBook-Pro.local>
2019-02-19 16:32:25 -0600

    Initial commit

---
+++ models/users.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+let userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+// Salt and hash the for the user's password
+userSchema.methods.setPassword = (password) => {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+// Check if the password is same as we have in our db
+userSchema.methods.validPassword = (password) => {
+  let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+// Generate JWT and sign for user
+userSchema.methods.generateJwt = () => {
+  let expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
